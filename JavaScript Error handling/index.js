ab0335c1bbf592df02ee5ae5cad5c2412a807573
Jayathu Shehan Dewmina <152476239+JayathuShehanDew@users.noreply.github.com>
2024-06-16 05:05:48 +0530

    up

---
+++ JavaScript Error handling/index.js
@@ -0,0 +1,24 @@
+// Error = An Object that is created to represent a problem that occurs
+//         Occur often with user input or establishing a connection
+
+// Errors happening,
+
+
+
+try{
+    console.log(x);
+    // NETWORK ERRORS
+    // PROMISE REJECTION
+    // SECURIITY ERRORS
+}
+catch(error){
+    console.error(error);
+}
+finally{
+    // CLOSE FILES
+    // CLOSE CONNECTION
+    // CLOSE RESOURCES
+    console.log("I am always executed");
+}
+
+console.log("You have reached the end!");
---
+++ What is asynchronous JavaScript code/index.js
@@ -0,0 +1,17 @@
+setTimeout(() => {console.log("Hello");}, 3000);
+
+console.log("Task 2");
+console.log("Task 3");
+console.log("Task 4");
+
+function func1(callback){
+    setTimeout(() => {console.log("Task 1"); callback();}, 3000);
+}
+
+function func2(){
+    console.log("Task 2");
+    console.log("Task 3");
+    console.log("Task 4");
+}
+
+func1(func2);
